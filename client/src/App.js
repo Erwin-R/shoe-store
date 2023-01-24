90fc47a5a8c98ae7d3108a6d8825b28a42622ae7
Bryan Park <bryan-j-park@outlook.com>
2023-01-24 10:28:06 -1000

    created multiple components and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import OrderSummary from './components/OrderSummary';
+import ProductList from './components/ProductList';
+import ProductPage from './components/ProductPage';
+import ShoppingCart from './components/ShoppingCart';
+import SideBar from './components/SideBar';
+import TrendingProduct from './components/TrendingProducts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route element={ <NavBar /> } path="/" />
+        <Route element={ <ProductPage /> } path="/product" />
+        <Route element={ <TrendingProduct />} path="/trending" />
+        <Route element={ <ProductList />} path="/products" />
+        <Route element={ <SideBar />} path="/sidebar" />
+        <Route element={ <ShoppingCart />} path="/cart" />
+        <Route element={ <OrderSummary />} path="/summary" />
+      </Routes>
     </div>
   );
 }
