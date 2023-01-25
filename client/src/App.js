5a451dd4a6073efc7a0f1f9c30c850459282032b
Bryan Park <bryan-j-park@outlook.com>
2023-01-24 17:39:13 -1000

    added more components and added context folder

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,64 @@ import NavBar from './components/NavBar';
 import OrderSummary from './components/OrderSummary';
 import ProductList from './components/ProductList';
 import ProductPage from './components/ProductPage';
-import ShoppingCart from './components/ShoppingCart';
 import SideBar from './components/SideBar';
 import TrendingProduct from './components/TrendingProducts';
+import ShoppingCartPage from './views/ShoppingCartPage';
+import ShoeContext from './context/ShoeContext';
 
 function App() {
+  const itemsInCart = [
+    {
+      id: 1,
+      name: 'ULTRABOOST 1.0',
+      href: '#',
+      price: '120.00',
+      color: 'Black',
+      inStock: true,
+      size: '9.5',
+      imageSrc: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/c608f554cb3b4d12b392af000188c513_9366/Ultraboost_1.0_Shoes_Black_HQ4199_01_standard.jpg',
+      imageAlt: "Front of men's Basic Tee in sienna.",
+    },
+    {
+      id: 2,
+      name: 'ULTRABOOST 1.0',
+      href: '#',
+      price: '200.00',
+      color: 'White',
+      inStock: false,
+      leadTime: '3–4 weeks',
+      size: '10',
+      imageSrc: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/12a7dc066e2640898c10af15018481f8_9366/Ultraboost_1.0_Shoes_White_HR0063_01_standard.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+    },
+    {
+      id: 3,
+      name: 'KAPTIR 2.0',
+      href: '#',
+      price: '90.00',
+      color: 'White',
+      inStock: true,
+      size: '8',
+      imageSrc: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/0846e90b15144861b33dacf500e3cfd1_9366/Kaptir_2.0_Shoes_White_H00276_01_standard.jpg',
+      imageAlt: 'Insulated bottle with white base and black snap lid.',
+    },
+  ]
+
+  const message = "hello"
+
   return (
     <div className="App">
-      <Routes>
-        <Route element={ <NavBar /> } path="/" />
-        <Route element={ <ProductPage /> } path="/product" />
-        <Route element={ <TrendingProduct />} path="/trending" />
-        <Route element={ <ProductList />} path="/products" />
-        <Route element={ <SideBar />} path="/sidebar" />
-        <Route element={ <ShoppingCart />} path="/cart" />
-        <Route element={ <OrderSummary />} path="/summary" />
-      </Routes>
+      <ShoeContext.Provider value={{itemsInCart, message}}>
+        <Routes>
+          <Route element={ <NavBar /> } path="/" />
+          <Route element={ <ProductPage /> } path="/product" />
+          <Route element={ <TrendingProduct />} path="/trending" />
+          <Route element={ <ProductList />} path="/products" />
+          <Route element={ <SideBar />} path="/sidebar" />
+          <Route element={ <ShoppingCartPage />} path="/cart" />
+          <Route element={ <OrderSummary />} path="/summary" />
+        </Routes>
+      </ShoeContext.Provider>
     </div>
   );
 }
