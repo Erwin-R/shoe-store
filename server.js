a733154528bac658da113faea9a72bfc8e07a739
Erwin-R <rerwin684@gmail.com>
2023-01-25 17:48:39 -0600

    added stripe dependencies, controller, and routes

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const cors = require('cors');
 const app = express();
 const port = 8000;
 
+require('dotenv').config();
+const Stripe = require("stripe")(process.env.SECRET_KEY);
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 require('./server/config/mongoose.config');
 require('./server/routes/shoe.routes')(app);
+require('./server/routes/stripe.routes')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+
