d0ec7fdbbb747517e393e9e08b80117c227e53c4
Gabriela Flores <gabrielaflores494@gmail.com>
2023-02-01 14:03:58 -0800

    styled login & reg pages

--- client/src/views/RegisterPage.jsx
+++ client/src/views/RegisterPage.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import RegisterForm from "../components/RegisterForm";
 import { useNavigate } from 'react-router-dom';
+import NewLogin from "../components/NewLogin";
 
 const RegisterPage = () => {
 
